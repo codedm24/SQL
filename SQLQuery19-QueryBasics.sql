f1554991805ff64db4a05671a595a9744a66d341
codedm24 <mithun.adroit@gmail.com>
2024-10-30 22:59:01 +0530

    SQL query basic
    updated

--- SQLQuery19-QueryBasics.sql
+++ SQLQuery19-QueryBasics.sql
@@ -1,3 +1,5 @@
+USE sampleABG;
+
 SELECT dept_name, dept_no
 FROM department
 WHERE location='Dallas'
@@ -79,36 +81,96 @@ FROM
 works_on
 GROUP BY project_no, job
 
-SELECT
-MIN(ALL emp_no)
-FROM
-employee
 
-SELECT
-MIN(DISTINCT emp_no)
-FROM
-employee
+---------------------------------------------------------------
+USE sampleABG;
 
---sub query eith aggregate function
-SELECT
-emp_lname, emp_no
-FROM
-employee
-WHERE emp_no = (
-SELECT MIN(emp_no) 
-FROM 
-employee)
+CREATE table dbo.table1(
+column1 INT PRIMARY KEY NOT NULL,
+column2 char(10));
 
---subquery
-SELECT 
-emp_no
-FROM
-works_on
-WHERE
-enter_date = (
-SELECT
-MAX(enter_date)
-FROM 
-works_on
-WHERE 
-job = 'Manager')
+INSERT INTO table1(column1,column2) 
+VALUES(NEXT VALUE FOR sequence1,'A')
+INSERT INTO table1(column1,column2)
+VALUES(NEXT VALUE FOR sequence1,'B')
+
+SELECT * FROM table1
+
+SELECT CURRENT_VALUE 
+FROM sys.sequences
+WHERE name='sequence1'
+
+DROP SEQUENCE sequence1
+------------------------------------------------------------------------
+USE sampleABG;
+SELECT emp_no, emp_fname, emp_lname, dept_no
+INTO employee_enh
+FROM employee;
+
+
+ALTER TABLE employee_enh
+	ADD domicile CHAR(25) NULL;
+
+SELECT * 
+FROM employee_enh;
+
+ALTER TABLE employee_enh
+	ALTER COLUMN domicile CHAR(30) NULL;
+
+ALTER TABLE employee_Enh
+	DROP COLUMN domicile;
+
+ALTER TABLE employee_enh
+	ADD domicile CHAR(25) NULL;
+
+UPDATE employee_enh
+SET domicile = 'San Antonio'
+WHERE emp_no = 25348;
+
+UPDATE employee_enh
+SET domicile = 'Houston'
+WHERE emp_no = 10102;
+
+UPDATE employee_enh
+SET domicile = 'San Antonio'
+WHERE emp_no = 18316;
+
+UPDATE employee_enh
+SET domicile = 'Seattle'
+WHERE emp_no = 29346;
+
+UPDATE employee_enh
+SET domicile = 'Portland'
+WHERE emp_no = 2581;
+
+UPDATE employee_enh
+SET domicile = 'Taacoma'
+WHERE emp_no = 9031;
+
+UPDATE employee_enh
+SET domicile = 'Houston'
+WHERE emp_no = 28559;
+
+SELECT domicile
+FROM employee_enh
+UNION ALL
+SELECT location
+FROM department
+-------------------------------------------------------------
+CREATE SEQUENCE dbo.sequence1
+AS INT
+START WITH 1
+INCREMENT BY 5
+MINVALUE 1 MAXVALUE 256
+CYCLE;
+
+SELECT NEXT VALUE FOR sequence1
+SELECT NEXT VALUE FOR sequence1
+
+ALTER SEQUENCE sequence1
+RESTART WITH 1;
+SELECT NEXT VALUE FOR sequence1
+SELECT NEXT VALUE FOR sequence1
+-----------------------------------------------------------
+
+
